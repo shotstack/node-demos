5971688d7dcaa8ead07451d4dc762c25230a71e4
Jeff Shillitto <jeff@shillitto.com>
2019-07-27 22:46:01 +1000

    Add text example

---
+++ examples/text.js
@@ -0,0 +1,70 @@
+const Shotstack = require('shotstack-sdk');
+
+const defaultClient = Shotstack.ApiClient.instance;
+const DeveloperKey = defaultClient.authentications['DeveloperKey'];
+const api = new Shotstack.DefaultApi()
+const id = process.argv[2];
+
+let apiUrl = 'https://api.shotstack.io/stage';
+
+if (!process.env.SHOTSTACK_KEY) {
+    console.log('API Key is required. Set using: export SHOTSTACK_KEY=your_key_here');
+    process.exit(1);
+}
+
+if (process.env.SHOTSTACK_HOST) {
+    apiUrl = process.env.SHOTSTACK_HOST;
+}
+
+defaultClient.basePath = apiUrl;
+DeveloperKey.apiKey = process.env.SHOTSTACK_KEY;
+
+let soundtrack = new Shotstack.Soundtrack;
+soundtrack
+    .setSrc('https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/music/disco.mp3')
+    .setEffect('fadeInFadeOut');
+
+let titleAsset = new Shotstack.TitleAsset;
+titleAsset
+    .setStyle('minimal')
+    .setText('Hello World');
+
+let title = new Shotstack.Clip;
+title
+    .setAsset(titleAsset)
+    .setStart(0)
+    .setLength(5)
+    .setEffect('zoomIn');
+
+let track = new Shotstack.Track;
+track
+    .setClips([title]);
+
+let timeline = new Shotstack.Timeline;
+timeline
+    .setBackground('#000000')
+    .setSoundtrack(soundtrack)
+    .setTracks([track]);
+
+let output = new Shotstack.Output;
+output
+    .setFormat('mp4')
+    .setResolution('sd');
+
+let edit = new Shotstack.Edit;
+edit
+    .setTimeline(timeline)
+    .setOutput(output);
+
+api.postRender(edit).then((data) => {
+    let message = data.response.message;
+    let id = data.response.id
+    
+    console.log(message + '\n');
+    console.log('>> Now check the progress of your render by running:');
+    console.log('>> node examples/status.js ' + id);
+
+}, (error) => {
+    console.error('Request failed: ', error);
+    process.exit(1);
+});
