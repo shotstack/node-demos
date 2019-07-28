10e2a60ecbaee52e5834738ea0936c2485ba8012
Jeff Shillitto <jeff@shillitto.com>
2019-07-28 16:32:26 +1000

    Add images example

--- examples/images.js
+++ examples/images.js
@@ -18,45 +18,38 @@ if (process.env.SHOTSTACK_HOST) {
 defaultClient.basePath = apiUrl;
 DeveloperKey.apiKey = process.env.SHOTSTACK_KEY;
 
-const styles = [
-    'minimal',
-    'blockbuster',
-    'vogue',
-    'sketchy',
-    'skinny',
-    'chunk',
-    'chunkLight',
-    'marker',
-    'future',
-    'subtitle'
+const images = [
+    'https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-712850.jpeg',
+    'https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-867452.jpeg',
+    'https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-752036.jpeg',
+    'https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-572487.jpeg',
+    'https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-114977.jpeg',
+    'https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-347143.jpeg',
+    'https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-206290.jpeg',
+    'https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-940301.jpeg',
+    'https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-266583.jpeg',
+    'https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-539432.jpeg'
 ];
 
 let clips = [];
 let start = 0;
-const length = 3;
+const length = 1.5;
 
 let soundtrack = new Shotstack.Soundtrack;
 soundtrack
-    .setSrc('https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/music/dreams.mp3')
+    .setSrc('https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/music/gangsta.mp3')
     .setEffect('fadeInFadeOut');
 
-styles.forEach((style) => {
-    let title = new Shotstack.TitleAsset;
-    title
-        .setStyle(style)
-        .setText(style);
-
-    let transition = new Shotstack.Transition;
-    transition
-        .setIn('fade')
-        .setOut('fade');
+images.forEach((image) => {
+    let imageAsset = new Shotstack.ImageAsset;
+    imageAsset
+        .setSrc(image);
 
     let clip = new Shotstack.Clip;
     clip
-        .setAsset(title)
+        .setAsset(imageAsset)
         .setStart(start)
         .setLength(length)
-        .setTransition(transition)
         .setEffect('zoomIn');
 
     start = start + length;
