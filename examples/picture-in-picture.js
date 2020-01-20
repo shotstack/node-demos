05348f73b3a5e40d156a06004fb4120e31a81f18
Jeff Shillitto <jeff@shillitto.com>
2020-01-20 12:05:55 +1100

    Fix indentation

--- examples/picture-in-picture.js
+++ examples/picture-in-picture.js
@@ -18,11 +18,12 @@ if (process.env.SHOTSTACK_HOST) {
 defaultClient.basePath = apiUrl;
 DeveloperKey.apiKey = process.env.SHOTSTACK_KEY;
 
-const oneFrame = 1/25;
+const ONE_FRAME = 1/25;
 
 // Setup the main background clip
 let backgroundAsset = new Shotstack.VideoAsset;
-backgroundAsset.setSrc('https://shotstack-pubic-files.s3-ap-southeast-2.amazonaws.com/examples/picture-in-picture/code.mp4');
+backgroundAsset
+    .setSrc('https://shotstack-pubic-files.s3-ap-southeast-2.amazonaws.com/examples/picture-in-picture/code.mp4');
 
 let backgroundClip = new Shotstack.Clip;
 backgroundClip
@@ -45,44 +46,44 @@ let overlayClip1 = new Shotstack.Clip;
 overlayClip1
     .setAsset(overlayAsset1)
     .setStart(0)
-    .setLength(5 - oneFrame);
+    .setLength(5 - ONE_FRAME);
 
 // 2. Bottom right picture in picture after 5 seconds
 let overlayAsset2 = new Shotstack.VideoAsset;
 overlayAsset2
-        .setSrc('https://shotstack-pubic-files.s3-ap-southeast-2.amazonaws.com/examples/picture-in-picture/commentary.mp4')
-        .setVolume(1)
-        .setTrim(5);
+    .setSrc('https://shotstack-pubic-files.s3-ap-southeast-2.amazonaws.com/examples/picture-in-picture/commentary.mp4')
+    .setVolume(1)
+    .setTrim(5);
 
 let overlayClip2 = new Shotstack.Clip;
 overlayClip2
     .setAsset(overlayAsset2)
     .setStart(5)
-    .setLength(5 - oneFrame)
+    .setLength(5 - ONE_FRAME)
     .setScale(0.35)
     .setPosition('bottomRight');
 
 // 3. Top right picture in picture after 10 seconds
 let overlayAsset3 = new Shotstack.VideoAsset;
 overlayAsset3
-        .setSrc('https://shotstack-pubic-files.s3-ap-southeast-2.amazonaws.com/examples/picture-in-picture/commentary.mp4')
-        .setVolume(1)
-        .setTrim(10);
+    .setSrc('https://shotstack-pubic-files.s3-ap-southeast-2.amazonaws.com/examples/picture-in-picture/commentary.mp4')
+    .setVolume(1)
+    .setTrim(10);
 
 let overlayClip3 = new Shotstack.Clip;
 overlayClip3
     .setAsset(overlayAsset3)
     .setStart(10)
-    .setLength(2.5 - oneFrame)
+    .setLength(2.5 - ONE_FRAME)
     .setScale(0.35)
     .setPosition('topRight');
 
 // 4. Small bottom right picture in picture after 12.5 seconds
 let overlayAsset4 = new Shotstack.VideoAsset;
 overlayAsset4
-        .setSrc('https://shotstack-pubic-files.s3-ap-southeast-2.amazonaws.com/examples/picture-in-picture/commentary.mp4')
-        .setVolume(1)
-        .setTrim(12.5);
+    .setSrc('https://shotstack-pubic-files.s3-ap-southeast-2.amazonaws.com/examples/picture-in-picture/commentary.mp4')
+    .setVolume(1)
+    .setTrim(12.5);
 
 let overlayClip4 = new Shotstack.Clip;
 overlayClip4
